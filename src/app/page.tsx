ebcf9ce2294af5fcaa6e709e3fe8553ee6b58e86
dominykasdev <contact@dominykas.dev>
2024-10-30 08:38:48 +0000

    checkpoint

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,10 +7,39 @@ import {
   Container,
   Typography,
 } from "@mui/material";
-import React from "react";
+import React, { useEffect } from "react";
 
 export default function Home() {
   const { user, isLoading } = useUser();
+
+  useEffect(() => {
+    if (user) {
+      // TODO:
+      // IF LOGGED IN, CHECK USER_ID IF EXISTS
+      // IF EXISTS, UPDATE LAST_LOGIN, LAST_IP
+      // ELSE REGISTER USER
+      console.log(user);
+
+      fetch("http://localhost:8080/api/user/" + user.sub)
+        .then((response) => response.json())
+        .then((data: unknown) => {
+          console.log(data);
+
+          // const body = {
+          //   email: user.email,
+          //   email_verified: user.email_verified,
+
+          // }
+
+          if (data == "[]") {
+            fetch("http://localhost:8080/api/user/", {
+              method: "POST",
+              body: JSON.stringify(user),
+            });
+          }
+        });
+    }
+  });
 
   return (
     <main>
